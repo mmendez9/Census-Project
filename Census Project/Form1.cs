b92569f4a19e520d0fbe3d5d23cc8e9afc71af0a
mmendez9 <hikari1995>
2016-11-16 10:29:27 -0500

    Data Added

--- Census Project/Form1.cs
+++ Census Project/Form1.cs
@@ -16,5 +16,22 @@ namespace Census_Project
         {
             InitializeComponent();
         }
+
+        private void addressBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.addressBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.myTownDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'myTownDBDataSet.PersonAddressView' table. You can move, or remove it, as needed.
+            this.personAddressViewTableAdapter.Fill(this.myTownDBDataSet.PersonAddressView);
+            // TODO: This line of code loads data into the 'myTownDBDataSet.Address' table. You can move, or remove it, as needed.
+            this.addressTableAdapter.Fill(this.myTownDBDataSet.Address);
+
+        }
     }
 }
